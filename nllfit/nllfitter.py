9f3a3a6d6bdaf9a927e73f6d3af0834b8a35ba36
Nathaniel Odell <naodell@gmail.com>
2017-09-11 11:22:55 -0500

    Implemented bootstrap based covariance estimation.  Moved verbose option from the fitter class memebers to an arguement for the fitter.fit method.

--- nllfit/nllfitter.py
+++ nllfit/nllfitter.py
@@ -21,12 +21,10 @@ class NLLFitter:
     model    : a Model object or and array of Model objects
     data     : the dataset or datasets we wish to carry out the modelling on
     min_algo : algorith used for minimizing the nll (uses available scipy.optimize algorithms)
-    verbose  : control verbosity of fit method
     '''
-    def __init__(self, model, min_algo='SLSQP', verbose=True):
+    def __init__(self, model, min_algo='SLSQP'):
         self._model    = model
         self.min_algo  = min_algo
-        self.verbose   = verbose
         self.aux_cost  = None
 
     def _objective(self, params, data):
@@ -61,9 +59,25 @@ class NLLFitter:
         x      : the data
         params : parameter values at which the Hessian will be evaluated.
         '''
-        pass
 
-    def _get_corr(self, x, params):
+        # Generate the pseudo-data (this could be done in several ways...)
+        rindex = np.random.randint(0, x.size, ntoys*x.size)
+        pdata  = np.array([x[i] for i in rindex])
+        pdata  = pdata.reshape(ntoys, x.size)
+
+        pparams = []
+        for i in range(ntoys):
+            r = self.fit(pdata[i], params, calculate_corr=False, verbose=False)
+            if r.status == 0:
+                pparams.append(r.x)
+
+        pparams = np.array(pparams).transpose()
+        cov = np.cov(pparams)
+        sig = np.sqrt(cov.diagonal())
+        corr_matrix = cov/np.outer(sig, sig)
+        return sig, corr_matrix
+
+    def _get_corr_hess(self, x, params):
         '''
         Calculates covariance matrix for model parameters by calculating the
         Hessian of the NLL conditioned on the dataset being fit to.
@@ -82,12 +96,11 @@ class NLLFitter:
 
         hobj = hcalc(params)[0]
         if np.linalg.det(hobj) != 0:
-            hinv        = np.linalg.pinv(hobj)
             # calculate the full covariance matrix in the case that the Hessian is non-singular
+            hinv        = np.linalg.pinv(hobj)
             sig         = np.sqrt(hinv.diagonal())
             corr_matrix = hinv/np.outer(sig, sig)
             return sig, corr_matrix
-
         else:
             print('Hessian matrix is singular! Cannot calculate covariance matrix of the likelihood')
             # if the Hessian is singular, try to just get its diagonal for parameter errors
@@ -96,7 +109,7 @@ class NLLFitter:
             return sig, corr_matrix
 
 
-    def fit(self, data, params_init=None, calculate_corr=True, mode='local'):
+    def fit(self, data, params_init=None, calculate_corr=True, cov_type='hess', verbose=True, mode='local'):
         '''
         Fits the model to the given dataset using scipy.optimize.minimize.
         Returns the fit result object.
@@ -109,11 +122,15 @@ class NLLFitter:
                          calculated.  If true, this will do a numerical calculation of the
                          covariance matrix based on the currenct objective function about the
                          minimum determined from the fit
+        cov_type       : specify how the covariance matrix is to be calculated; current options are:
+                            'hess': this will use the inverse of the Hessian matrix for calculating the covariance (default)
+                            'bootstrap': bootstraps the input dataset to estimate the covariance
+        verbose        : controls whether fit results are printed to stdout
         mode           : determines whether optimize.minize ('local') or optimize.basinhopping ('global') is used
         '''
 
-        if params_init:
-            self._model.update_params(params_init)
+        if not isinstance(params_init, type(None)):
+            self._model.update_parameters(params_init)
         else:
             params_init = self._model.get_parameters(by_value=True)
 
@@ -148,18 +165,20 @@ class NLLFitter:
             else:
                 result.status = -1
 
-        if self.verbose:
+        if verbose:
             print('Fit finished with status: {0}\n'.format(result.status))
 
         if result.status == 0:
             if calculate_corr:
-                sigma, corr = self._get_corr(data, result.x)
-                #sigma, corr = self._get_corr_mc(data, result.x)
+                if cov_type == 'bootstrap':
+                    sigma, corr = self._get_corr_mc(data, result.x)
+                else:
+                    sigma, corr = self._get_corr_hess(data, result.x)
             else:
-                sigma, corr = result.x, 0.
+                sigma, corr = result.x, np.identity(np.size(result.x))
 
             self._model.update_parameters(result.x, (sigma, corr))
-            if self.verbose:
+            if verbose:
                 report_fit(self._model.get_parameters(), show_correl=False)
                 #report_fit(result, show_correl=False)
                 print('')
